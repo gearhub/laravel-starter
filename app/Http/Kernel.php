3668c4292f0494f301de469d3bb255b4a0039dda
Vince Ruby <vsruby91@gmail.com>
2016-08-03 16:49:06 -0400

    Update to Laravel 5.3
    ab453a7e82fcb348eae7bc453fddf321a4741db4

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        // \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
     ];
 
     /**
@@ -29,10 +29,12 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             // \App\Http\Middleware\VerifyCsrfToken::class,
+            // \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
-            // 'throttle:60,1',
+            'throttle:60,1',
+            'bindings',
         ],
     ];
 
@@ -45,5 +47,11 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\AuthenticateJwt::class,
+        // 'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+        // 'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,25 +1,5 @@
 <?php
 
-if (! function_exists('array_keys_to_snake')) {
-
-    /**
-     * Convert the keys of an array to snake case.
-     *
-     * @param  array $array
-     *
-     * @return array
-     */
-    function array_keys_to_snake($array)
-    {
-        return array_build($array, function($key, $value) {
-            return [
-                snake_case($key),
-                $value
-            ];
-        });
-    }
-}
-
 if (! function_exists('generate_uuid')) {
 
     /**
