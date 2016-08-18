e3788e4b59bab075411d5238c56fd2512c280553
Vince Ruby <vsruby91@gmail.com>
2016-08-18 10:41:17 -0400

    Remove reference to a facade that is no longer installed

--- config/app.php
+++ config/app.php
@@ -227,7 +227,6 @@ return [
 
         'JWTAuth'      => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory'   => Tymon\JWTAuth\Facades\JWTFactory::class,
-        'Uuid'         => Webpatser\Uuid\Uuid::class,
     ],
 
 ];
